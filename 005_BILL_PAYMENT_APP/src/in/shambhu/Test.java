27c355ca05a526c746f71007176d3da330182099
shamnitjsr <shamnitjsr@gmail.com>
2022-12-16 02:14:25 +0530

    'setter'

--- 005_BILL_PAYMENT_APP/src/in/shambhu/Test.java
+++ 005_BILL_PAYMENT_APP/src/in/shambhu/Test.java
@@ -4,14 +4,15 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		//BillCollector bc = new BillCollector();
+		BillCollector bc = new BillCollector();
 		
 		//injecting CreditCard obj into BillCollector (Setter Injection)
-		//bc.setPayment(new CreditcardPayment());
+		bc.setPayment(new CreditcardPayment());
+		bc.collectPayment(1200.89);
 		
 		
 		//injecting CreditCard obj into BillCollector (Constructor Injection)
-		BillCollector bc1 = new BillCollector(new CreditcardPayment());
+		BillCollector bc1 = new BillCollector(new DebitcardPayment());
 		bc1.collectPayment(1500.09);
 
 	}
